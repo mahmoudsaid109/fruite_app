2993d168b3f3a63d6e952f95187bc1f47fc60c7b
Mahmoud Said <102619410+mahmoudsaid109@users.noreply.github.com>
2025-12-17 20:53:10 +0200

    add firebase files

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:fruite_app/core/helper_functions/on_generate_route.dart';
 import 'package:fruite_app/core/services/shared_prefrences_senglton.dart';
 import 'package:fruite_app/core/utils/app_colors.dart';
+import 'package:fruite_app/firebase_options.dart';
 import 'package:fruite_app/generated/l10n.dart';
 import 'features/splash/presentation/views/splash_screen.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await Prefs.init();
   runApp(const FruitHub());
 }
